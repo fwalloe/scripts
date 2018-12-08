c7a2b1e94785a9b095a67d0c830c7db513a502c5
Fredrik Walloe <code@fredrikwalloe.com>
2018-12-08 14:32:11 +0100

    Fixed documentation

--- findAll.sh
+++ findAll.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+####
 ##
-#
-# findAll: searches for a pattern in all files in the current directory. 
+## findAll.sh: searches for a pattern in all files in the current directory. 
 # Uses /dev/null to trick grep into displaying filenames where pattern is found
-#
-# by Fredrik Walloe
-#
 ##
+## by Fredrik Walloe
+##
+####
 
 cat *.* | grep --color -E $1 *.* /dev/null
 
