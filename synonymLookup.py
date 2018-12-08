fe66a7e7e183b72856b6d5f144f88cf8c44beb4c
Fredrik Walloe <code@fredrikwalloe.com>
2018-12-08 14:52:11 +0100

    Fixed and added documentation

--- synonymLookup.py
+++ synonymLookup.py
@@ -1,9 +1,12 @@
 #!/bin/python
 
-###
-# synonymLookup.py: lets users find synonyms for the words they pass to the script as arguments
-# usage: ./synonymLookup.py WORD
-###
+####
+##
+## synonymLookup.py: lets users find synonyms for the words they pass to the script as arguments; can fail if a word has multiple possible meanings. 
+## usage: ./synonymLookup.py WORD
+## Note: if this stops working that likley means the class name for synonyms have changed on the website. Use the Inspect tool in your browser to find the correct class name for the synonyms 
+## 
+####
 
 # encoding errors can be fixed  by exporting lc_all with utf-8
 from bs4 import BeautifulSoup
@@ -18,7 +21,7 @@ soup.prettify()
 
 print ("Synonyms for " + sys.argv[1] + ":")
 synonymNumber = 1
-for definition in soup.find_all("a", class_="css-ebz9vl e1s2bo4t1"):
+for definition in soup.find_all("a", class_="css-3kshty etbu2a31"):
     print (str(synonymNumber) + ": " + str(definition.text).strip(), " ", )
     synonymNumber += 1
 
